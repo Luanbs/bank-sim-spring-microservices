e4056379079f641f42d2a8f3bcc1ca678acee3cd
Luanbs <luankraika@hotmail.com>
2026-01-29 13:43:27 -0300

    Initial functional project with docker and gateway

---
+++ account-service/src/main/java/com/banksim/account_service/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.banksim.account_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "account ok";
+    }
+}
---
+++ auth-service/src/main/java/com/banksim/auth_service/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.banksim.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "auth ok";
+    }
+}
